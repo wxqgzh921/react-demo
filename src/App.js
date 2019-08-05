aac8b642fd64e2441c3b373b4e178b7bce9d4c2e
webgzh907247189 <18255106634@163.com>
2019-08-05 14:03:23 +0800

    update

--- src/App.js
+++ src/App.js
@@ -17,24 +17,60 @@ function a(user){
   return <h1>hello,world</h1>
 }
 
-function tick(){
+function Tick(){
   const time = new Date().toLocaleTimeString()
   return time
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <h1>hello,{formateName(user)}</h1> */}
-        {a(user)}
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {tick()}
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {num: 1}
+
+  componentDidMount(){
+    // setInterval(()=>{
+    // },1000)
+  }
+
+  handleClick = () =>{
+    // this.setState({num: this.state.num + 1})
+    // console.log(this.state.num)
+
+    // setTimeout(() => {
+    //   console.log(this.state.num,'setTimeout')
+    // }, 2000);
+
+
+
+
+
+    // this.setState({num: this.state.num + 1},()=>{
+    //   console.log(this.state.num)
+    // })
+
+
+
+    this.setState((state)=>{
+      console.log(state,'111')
+      return {num: this.state.num + 1}
+    })
+  }
+
+  render(){
+    console.log('render')
+    return (
+      <div className="App">
+        <button onClick={this.handleClick}>增加</button>
+        <header className="App-header">
+          {/* <h1>hello,{formateName(user)}</h1> */}
+          {a(user)}
+          {this.state.num}
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Tick/>
+        </header>
+      </div>
+    );
+  }
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ import * as serviceWorker from './serviceWorker';
 // ReactDOM.render(element,document.getElementById('root'))
 
 ReactDOM.render(<App />, document.getElementById('root'));
-setInterval(App,1000)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
